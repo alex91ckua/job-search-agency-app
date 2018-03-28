dc3606cd44038c399bd117270e9f9a3b158d7bf7
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-28 17:45:15 +0300

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :jobs, only: [:index, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :companies, only: [:index, :show]
+  resource :candidate, only: [:create]
 end
