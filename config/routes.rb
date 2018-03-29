2b9426e701f2273c30f2ab55d1a56ef668eb99d0
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-29 17:36:47 +0300

    callback form added on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  post '/home', to: 'home#create_callback', as: :create_callback
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :jobs, only: [:index, :show]
