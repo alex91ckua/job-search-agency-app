66a7fb60609e460ff9a9c20ac1e4cecc70e46e22
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-29 17:39:52 +0300

    CallBackForm model added, based on MailForm

---
+++ app/models/callback_form.rb
@@ -0,0 +1,20 @@
+class CallbackForm < MailForm::Base
+  attribute :first_name,      :validate => true
+  attribute :last_name,      :validate => true
+  attribute :request_type,     :validate => ["General", "Propose"]
+  attribute :phone,      :validate => true
+  attribute :company_name,      :validate => true
+  attribute :call_time,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+        :subject => "Contact You",
+        :to => "your_email@your_domain.com",
+        :from => %("#{first_name} #{last_name}" <#{email}>)
+    }
+  end
+end
