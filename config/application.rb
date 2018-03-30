383275a814cbb2eadb83c509f31d6810aa216b65
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-31 01:15:12 +0300

    app version added

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module GlobalAccounting
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    VERSION = "1.0"
   end
 end
