3e6d31e956150ddea913a2e5e9012e61b180126f
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-30 15:31:57 +0300

    request callback form moved to appropriate controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,4 @@ class HomeController < ApplicationController
   def index
     @callback = CallbackForm.new
   end
-
-  def create_callback
-    @callback = CallbackForm.new(params['callback_form'])
-    @callback.request = request
-    if @callback.deliver
-      @callback = CallbackForm.new
-      flash.now[:success] = 'Thank you for your message!'
-    else
-      flash.now[:error] = 'Cannot send message.'
-    end
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root 'home#index'
-  post '/home', to: 'home#create_callback', as: :create_callback
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :jobs, only: [:index, :show]
   resources :companies, only: [:index, :show]
   resource :candidate, only: [:create]
+  get 'client-services', to: 'client_services#index'
+  scope 'client-services' do
+    get 'request-callback', to: 'request_callback#index'
+    post 'request-callback', to: 'request_callback#create'
+  end
+
 end
