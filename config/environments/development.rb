fffaeb78b0b18b0664147b2353427afc88211c30
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-03-31 17:29:04 +0300

    letter_opener gem added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+  # Open email with letter opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
