ab1a79bdf4f665f8a75b5f4d11996363241f7121
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-04-03 14:59:12 +0300

    - image uploader added to Article model - "where tag is" scope added to Article model - filtration added to model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,15 @@ class ArticlesController < InheritedResources::Base
 
   def index
     @articles = Article.all
+    @articles = @articles.where_tag(params[:order_by_tag]) if params[:order_by_tag].present?
+    tags = Article.pluck(:tags)
+    @striped_tags = []
+    tags.each { |t| t.split(',').each { |tag| @striped_tags.push tag.strip } }
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @related_articles = Article.where.not(id: params[:id]).limit(9)
   end
 
   private
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :tags, presence: true
+  mount_uploader :image, AttachmentUploader
   belongs_to :admin_user
+  scope :where_tag, ->(tag) { where('tags ilike ?', "%#{tag}%") }
+
 end
