43f2b751f735a98ad68a7d7d733d72771b64f52c
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-04-03 11:31:21 +0300

    - first and last name added - change password only if it's populated

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,11 +1,23 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :first_name, :last_name
   menu parent: 'Settings'
+
+  controller do
+    def update
+      model = :admin_user
+      if params[model][:password].blank?
+        %w(password password_confirmation).each { |p| params[model].delete(p) }
+      end
+      super
+    end
+  end
 
   index do
     selectable_column
     id_column
     column :email
+    column :first_name
+    column :last_name
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -20,6 +32,8 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs do
       f.input :email
+      f.input :first_name
+      f.input :last_name
       f.input :password
       f.input :password_confirmation
     end
