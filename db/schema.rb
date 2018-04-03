7a7bee5aad462b5d4c0308e8c717398d98bc2817
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-04-03 15:00:29 +0300

    image added to Article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402162742) do
+ActiveRecord::Schema.define(version: 20180403085418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180402162742) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "subtitle"
+    t.string "image"
     t.index ["admin_user_id"], name: "index_articles_on_admin_user_id"
   end
 
