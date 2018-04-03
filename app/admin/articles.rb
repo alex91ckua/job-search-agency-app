74119f1b899a6691a2bdc0271da7841567e686ad
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-04-03 14:56:56 +0300

    add image tag on active admin resource page

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,11 +1,16 @@
 ActiveAdmin.register Article do
-  permit_params :title, :subtitle, :description, :tags, :admin_user_id
+  permit_params :title, :subtitle, :description, :tags, :admin_user_id, :image
   menu label: 'Blog'
 
   index do
     column :id
     column :title
     column :subtitle
+    column :image do |a|
+      if a.image.url
+        image_tag a.image.url, width: 100
+      end
+    end
     column :description do |a|
       truncate(a.description, length: 100)
     end
@@ -17,10 +22,34 @@ ActiveAdmin.register Article do
     actions
   end
 
+  show do
+    attributes_table do
+      row :id
+      row :title
+      row :subtitle
+      row :image do |a|
+        if a.image.url
+          image_tag a.image.url, width: 200
+        end
+      end
+      row :description
+      row :tags
+      row :admin_user do |a|
+        link_to "#{a.admin_user.first_name} #{a.admin_user.last_name}", admin_admin_user_path(a.admin_user.id)
+      end
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
   form do |f|
     inputs do
       f.input :title
       f.input :subtitle
+      f.input :image, :as => :file, :hint => f.object.image.present? ? \
+                                              image_tag(f.object.image.url, width: 100) : \
+                                              content_tag(:span, 'no image selected')
       f.input :description, as: :froala_editor, input_html: {data: {options: {height: 300}}}
       f.input :tags
       f.input :admin_user_id, as: :select, :collection => Hash[AdminUser.all.map{|u| ["#{u.first_name} #{u.last_name}",u.id]}]
