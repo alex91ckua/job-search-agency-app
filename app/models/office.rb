d8828c88116d3127b37cda2eae6e2431fa94494e
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-04-05 13:39:50 +0300

    Office and Staff model validation added

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,2 +1,6 @@
 class Office < ApplicationRecord
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :work_time, presence: true
+  validates :address, presence: true
 end
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,3 +1,6 @@
 class Staff < ApplicationRecord
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :description, presence: true
   mount_uploader :image, AttachmentUploader
 end
