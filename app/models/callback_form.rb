6e059ca2f59159de4a80fbb7631b0d0b6426cfda
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-04-10 11:26:43 +0300

    Callback mailform validation fixes

--- app/models/callback_form.rb
+++ app/models/callback_form.rb
@@ -1,13 +1,15 @@
 class CallbackForm < MailForm::Base
-  attribute :first_name,      :validate => true
-  attribute :last_name,      :validate => true
-  attribute :request_type,     :validate => ["General", "Propose"]
-  attribute :phone,      :validate => true
-  attribute :company_name,      :validate => true
-  attribute :call_time,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :first_name, validate: true
+  attribute :last_name, validate: true
+  attribute :request_type, validate: %w[General Purpose]
+  attribute :phone, validate: true
+  attribute :company_name
+  attribute :call_time
+  attribute :email,
+            validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i,
+            allow_blank: true
   attribute :message
-  attribute :nickname,   :captcha => true
+  attribute :nickname, captcha: true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
