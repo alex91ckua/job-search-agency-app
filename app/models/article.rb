a044af5efd7cd993e523d7f070de94316b7809b2
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-04-14 20:25:25 +0300

    tags are not required

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
-  validates :tags, presence: true
+  # validates :tags, presence: true
   mount_uploader :image, AttachmentUploader
   belongs_to :admin_user
   scope :where_tag, ->(tag) { where('tags ilike ?', "%#{tag}%") }
