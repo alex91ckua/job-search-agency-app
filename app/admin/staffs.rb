eb4c6f4276cdccd6eee30a970a8c0c261a9d9e0d
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-04-25 16:37:46 +0300

    wysiwyg editor added to staff description

--- app/admin/staffs.rb
+++ app/admin/staffs.rb
@@ -1,4 +1,22 @@
 ActiveAdmin.register Staff do
 permit_params :name, :image, :description
 menu parent: 'Settings'
+
+  form do |f|
+    inputs do
+      f.input :name
+      f.input :image, :as => :file, :hint => f.object.image.present? ? \
+                                                image_tag(f.object.image.url, width: 100) : \
+                                                content_tag(:span, 'no image selected')
+      f.input :description, as: :froala_editor, input_html:
+          {
+              data: {
+                  options: {
+                      height: 300
+                  }
+              }
+          }
+    end
+    f.actions
+  end
 end
