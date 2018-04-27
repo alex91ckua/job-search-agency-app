f1e4ef820dd9e619349a06b0b7c75cbcab02ec57
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-04-27 09:45:33 +0300

    gmail smtp credentials moved to secrets.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,12 +89,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Gmail SMTP
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
       :authentication => :plain,
       :domain => "gmail.com",
-      :user_name => ENV['GMAIL_SMTP_USERNAME'],
-      :password => ENV['GMAIL_SMTP_PASSWORD'],
+      :user_name => rails.application.secrets[:gmail_smtp_username],
+      :password => rails.application.secrets[:gmail_smtp_password]
   }
 end
