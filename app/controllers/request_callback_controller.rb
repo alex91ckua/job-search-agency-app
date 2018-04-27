2cc9f363a6ce3caa3a5a59e14c22d0c7eed33ae5
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-04-27 10:04:50 +0300

    catch callback error

--- app/controllers/request_callback_controller.rb
+++ app/controllers/request_callback_controller.rb
@@ -6,13 +6,18 @@ class RequestCallbackController < ApplicationController
   def create
     @callback = CallbackForm.new(params['callback_form'])
     @callback.request = request
-    if @callback.deliver
-      @callback = CallbackForm.new
-      flash.now[:success] = 'Thank you for your message!'
-      status_code = 200
-    else
-      flash.now[:error] = @callback.errors.full_messages.to_sentence
-      status_code = 400
+    begin
+      if @callback.deliver
+        @callback = CallbackForm.new
+        flash.now[:success] = 'Thank you for your message!'
+        status_code = 200
+      else
+        flash.now[:error] = @callback.errors.full_messages.to_sentence
+        status_code = 400
+      end
+    rescue => ex
+      flash.now[:error] = "Sorry, something bad occur. Error: #{ex.message}"
+      # logger.error ex.message
     end
     respond_to do |format|
       format.html { render :index, status: status_code }
