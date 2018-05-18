383570d8fe82a511edd02506e732a1e9870a6eff
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-05-18 19:07:27 +0300

    candidate apply email form view updated

--- app/mailers/candidate_form_mailer.rb
+++ app/mailers/candidate_form_mailer.rb
@@ -2,6 +2,7 @@ class CandidateFormMailer < ApplicationMailer
   def candidate_apply_mail
     @candidate = params[:candidate]
     @url = params[:url]
-    mail(to: Setting.email, subject: "New candidate application for job #{@candidate.job.title}")
+    mail(to: Setting.email,
+         subject: "New candidate application for job #{@candidate.job.title}")
   end
 end
