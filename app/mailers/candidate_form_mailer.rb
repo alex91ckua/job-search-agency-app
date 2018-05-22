f21c8748308d87951773e0ebb91b1136327d5c05
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-05-22 13:02:45 +0300

    Attach CV to admin candidate inquire email

--- app/mailers/candidate_form_mailer.rb
+++ app/mailers/candidate_form_mailer.rb
@@ -1,7 +1,10 @@
 class CandidateFormMailer < ApplicationMailer
   def candidate_apply_mail
     @candidate = params[:candidate]
-    @url = params[:url]
+    @candidate_admin_url = params[:url]
+    unless @candidate.attachment.file.nil?
+      attachments[@candidate.attachment.file.filename.to_s] = @candidate.attachment.read
+    end
     mail(to: Setting.email,
          subject: "New candidate application for job #{@candidate.job.title}")
   end
