11c8386b330fe0e0b52ef78093b529516344a304
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-05-23 14:54:22 +0300

    attachment is required on register candidate form

--- app/models/register_candidate_form.rb
+++ app/models/register_candidate_form.rb
@@ -4,7 +4,7 @@ class RegisterCandidateForm < MailForm::Base
   attribute :phone, validate: true
   attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :job_alerts
-  attribute :attachment, attachment: true
+  attribute :attachment, validate: true, attachment: true
   attribute :nickname, captcha: true
 
   # Declare the e-mail headers. It accepts anything the mail method
