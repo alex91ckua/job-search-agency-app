1dff3f66e75b1866e13683455880aa224039e7a8
Alex Masliychuk <alexmasliychuk@gmail.com>
2018-11-14 20:12:24 +0200

    update slug on model edit

--- app/models/landing_page.rb
+++ app/models/landing_page.rb
@@ -3,4 +3,8 @@ class LandingPage < ApplicationRecord
   friendly_id :title, use: :slugged
 
   validates :title, presence: true
+
+  def should_generate_new_friendly_id?
+    title_changed? || new_record? || slug.blank?
+  end
 end
