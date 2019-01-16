c861ace74d7f9040f621e6171dd79ec96bcea106
Alex Masliychuk <alexmasliychuk@gmail.com>
2019-01-16 21:52:37 +0200

    text files are allowed only on candidate forms

--- app/models/register_landing_candidate_form.rb
+++ app/models/register_landing_candidate_form.rb
@@ -5,7 +5,7 @@ class RegisterLandingCandidateForm < MailForm::Base
   attribute :phone, validate: true
   attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :job_alerts
-  attribute :attachment, validate: true, attachment: true
+  attribute :attachment, attachment: true, validate: :file_type_allowed?
   attribute :nickname, captcha: true
 
   # Declare the e-mail headers. It accepts anything the mail method
@@ -16,5 +16,19 @@ class RegisterLandingCandidateForm < MailForm::Base
       to: Setting.landing_form_recipients,
       from: %("#{first_name} #{last_name}" <#{email}>)
     }
+  end
+
+  def file_type_allowed?
+    acceptable_types = [
+      'application/pdf',
+      'application/vnd.ms-excel',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'application/msword',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'text/plain'
+    ]
+    unless acceptable_types.include? attachment.content_type.chomp
+      self.errors.add(:attachment, "with type PDF, EXCEL, WORD or TEXT files are allowed.")
+    end
   end
 end
--- app/models/register_vacancy_form.rb
+++ app/models/register_vacancy_form.rb
@@ -5,7 +5,7 @@ class RegisterVacancyForm < MailForm::Base
   attribute :company_name, validate: true
   attribute :role, validate: true
   attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :attachment, attachment: true
+  attribute :attachment, attachment: true, validate: :file_type_allowed?
   attribute :nickname, captcha: true
 
   # Declare the e-mail headers. It accepts anything the mail method
@@ -16,5 +16,19 @@ class RegisterVacancyForm < MailForm::Base
       to: Setting.email,
       from: %("#{first_name} #{last_name}" <#{email}>)
     }
+  end
+
+  def file_type_allowed?
+    acceptable_types = [
+      'application/pdf',
+      'application/vnd.ms-excel',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'application/msword',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'text/plain'
+    ]
+    unless acceptable_types.include? attachment.content_type.chomp
+      self.errors.add(:attachment, "with type PDF, EXCEL, WORD or TEXT files are allowed.")
+    end
   end
 end
